9cfc2df33094e00a89cd8432ed24880a5ca3793d
hanzputro <hanzputro@gmail.com>
2024-02-10 07:55:29 +0700

    add search feature from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import ngMusic from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-t from-[#712bda] to-[#a45deb]">
       <div className="flex justify-center items-center h-[80vh]">
         <img src={ngMusic} width={72.2} />
       </div>
       <div className="w-full max-w-[280px] h-[20vh] flex flex-col justify-center space-y-[15px]">
-        <input className="input" placeholder="Artist / Album / Title" />
-        <Link to="/result" className="button">
+        <input
+          className="input"
+          placeholder="Artist / Album / Title"
+          onChange={(input: any) => setSearch(input.target.value)}
+        />
+        <Link to="/result" state={{ search: search }} className="button">
           Search
         </Link>
       </div>
